5e0dd861917bbcedeed55b8a7c07f73c02127296
Adarsh Gouda <goudaadu146@gmail.com>
2026-09-20 09:34:09 +0530

    exception with else concept

---
+++ exceptionwithelse.py
@@ -0,0 +1,8 @@
+try:
+    a=int(input("enter the first number:"))
+    b=int(input("enter the second number:"))
+    print(a/b)
+except ZeroDivisionError:
+    print("cannot divided by zero!..")
+else:
+    print("division successfull..")
