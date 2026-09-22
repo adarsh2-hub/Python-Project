f922746f9170c2d969cf6c3bff505262d79d2a31
Adarsh Gouda <goudaadu146@gmail.com>
2026-09-22 20:46:10 +0530

    Python_Projects_2

--- dundermethod2.py
+++ dundermethod2.py
@@ -1,9 +1,9 @@
-class Student:
-    def __init__(self,name,age,course):
-        self.name=name
-        self.age=age
-        self.course=course
+class Book:
+    def __init__(self,title,author,price):
+        self.title=title
+        self.author=author
+        self.price=price
     def __str__(self):
-        return f"Name : {self.name} , Age : {self.age} and Course: {self.course}"
-student=Student("Adarsh",21,"BCA")
-print(student)
+        return f"Title : {self.title} , Author : {self.author} and Price: {self.price}"
+book=Book("Python Basics","John",500)
+print(book)
